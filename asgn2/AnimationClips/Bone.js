fc07f831e74ad8d108829e088d16c94d80792643
Arjun Krishnan <akrish29@ucsc.edu>
2025-04-26 01:54:37 -0700

    day done

---
+++ asgn2/AnimationClips/Bone.js
@@ -0,0 +1,2 @@
+// TODO: add keyframes that match up to the bird eating the bone
+
---
+++ asgn2/AnimationClips/Default.js
@@ -0,0 +1,5 @@
+// TODO: add two keyframes, one to start flapping and one to stop
+
+// TODO: add a way to randomly select a x pos and y pos, maybe add z head pos to the animations?
+
+// TODO: add an animation to pick up and eat the bone
